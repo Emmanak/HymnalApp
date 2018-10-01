42e9c27a0cf500e392005ee826fcca70ae00633a
Emmanuel Augustine <emmanuelaugustine@hotmail.com>
2018-10-01 12:09:15 -0700

    Hymnal app version 1 with no downloads

---
+++ hymnalApp/app/src/main/java/augustine/emmanuel/hymnalnet/MainActivity.java
@@ -0,0 +1,34 @@
+package augustine.emmanuel.hymnalnet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.hymnal.net/en/home");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
